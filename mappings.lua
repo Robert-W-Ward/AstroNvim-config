ca76357c9e75ab20a61412b96a09f19baf2171ae
Robert Ward <robertwesleyward2019@gmail.com>
2023-06-14 11:51:25 -0500

    disabled arrow keys in normal mode and remapped Ctrl-Alt(HJKL) to the arrow keys in insert mode

--- mappings.lua
+++ mappings.lua
@@ -15,14 +15,28 @@ return {
       end,
       desc = "Pick to close",
     },
+        
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<Up>"] = {"<Nop>",desc = "No-op"},
+    ["<Down>"] = {"<Nop>",desc = "No-op"},
+    ["<Left>"] = {"<Nop>",desc = "No-op"},
+    ["<Right>"] = {"<Nop>",desc = "No-op"},
+
+    
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  i = {
+    
+    ["<C-M-h>"] = {"<Left>",desc = "Move Left in insert mode"},
+    ["<C-M-j>"] = {"<Down>",desc = "Move Down in insert mode"},
+    ["<C-M-k>"] = {"<Up>",desc = "Move Up in insert mode"},
+    ["<C-M-l>"] = {"<Right>",desc = "Move Right in insert mode"},
+  }
 }
